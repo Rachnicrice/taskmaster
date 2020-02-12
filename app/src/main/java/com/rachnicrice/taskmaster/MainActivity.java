be6d1d93b5b6695618563e003915e425d961e78d
Rachael Rice <Rachnicrice@gmail.com>
2020-02-11 20:47:02 -0800

    Added styling to settings and task detail activities

--- app/src/main/java/com/rachnicrice/taskmaster/MainActivity.java
+++ app/src/main/java/com/rachnicrice/taskmaster/MainActivity.java
@@ -17,15 +17,25 @@ public class MainActivity extends AppCompatActivity {
         View b1 = findViewById(R.id.button1);
         View b2 = findViewById(R.id.button2);
 
+        View set = findViewById(R.id.settings);
+
+        View t1 = findViewById(R.id.task1);
+        View t2 = findViewById(R.id.task2);
+        View t3 = findViewById(R.id.task3);
+
         //Set up the event listeners
         b1.setOnClickListener((v) -> {
-            Intent in = new Intent(this, AddTask.class);
-            startActivity(in);
+            Intent i = new Intent(this, AddTask.class);
+            startActivity(i);
         });
 
         b2.setOnClickListener((v) -> {
             Intent i = new Intent(this, AllTasks.class);
             startActivity(i);
+        });
+
+        set.setOnClickListener( (v) -> {
+            Intent i = new Intent(this, Settings.class);
         });
     }
 }
